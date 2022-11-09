122e73384102fb16307264caa4d62d8165055f39
maulana <maulanakhalid27@gmail.com>
2022-11-09 15:06:37 +0700

    BELUM SELESAI

--- src/UTS_1D.java
+++ src/UTS_1D.java
@@ -1,5 +1,8 @@
 public class UTS_1D {
     public static void(String[]args){
         System.out.println("hello class 1D");
+        for (int i=0;i<10;i++){
+            System.out.println("Hallo");
+        }
     }
 }
